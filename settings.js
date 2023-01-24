e8e17457775bd98c8169c4fd6773cfc8a556daec
adeesha7855 <123428009+adeesha7855@users.noreply.github.com>
2023-01-24 18:44:07 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,23 +2,23 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 
-global.botname = "Queen Anya" 
+global.botname = "𓄂⍣⃝🇶‌𝚞𝚎𝚎𝚗 🇦𝚗𝚢𝚊  𓆩×͜×𓆪" 
 global.botLanguage = "default" // 𝗗𝗲𝗳𝗮𝘂𝗹𝘁 || 𝗘𝗻𝗴𝗹𝗶𝘀𝗵 || 𝗛𝗶𝗻𝗱𝗶 || 𝗔𝗿𝗮𝗯𝗶𝗰 \\
-global.author = "𝙋𝙄𝙆𝘼"  
-global.packname = "𝙌𝙐𝙀𝙀𝙉✯𝘼𝙉𝙔𝘼✯𝘽𝙊𝙏"  
+global.author = "𓄂⍣⃝🇦𝚗𝚢𝚊  𓆩×͜×𓆪"  
+global.packname = "𓄂⍣⃝🇶‌𝚞𝚎𝚎𝚗 🇦𝚗𝚢𝚊  𓆩×͜×𓆪"  
 global.myweb = "https://github.com/PikaBotz" // You can put any link 
 global.waterMark = "×͜× King ✘ Pika™ ✓" // Your own watermark 
 global.headerText = "Anya by Pika" // text for header of menu         
 global.DocumentTitle = "MASTER⚔️PIKA" 
 
 
-global.ownername = "𝕻𝖎𝖐𝖆♰𝕶𝖚𝖓"   // Owner name //        
-global.ownernumber = ['916900904828'] 
+global.ownername = "𓄂⍣⃝🇶‌𝚞𝚎𝚎𝚗 🇦𝚗𝚢𝚊  𓆩×͜×𓆪"   // Owner name //        
+global.ownernumber = ['+94716132814'] 
 global.socialm = "𝗜𝗻𝘀𝘁𝗮 : 3.69_pika" // 𝗜𝗻𝘀𝘁𝗮 : || 𝗚𝗶𝘁𝗵𝘂𝗯 : || 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 : \\
-global.ytname = "YT: Ytname"   // Yt chanel name //
+global.ytname = "YT: Sawana TV"   // Yt chanel name //
 global.continent = "Asia" // your continent name 
-global.region = "India" // your country name
-global.state = "Assam" // your state name
+global.region = "Sri Lanka" // your country name
+global.state = "𓄂⍣⃝🇶‌𝚞𝚎𝚎𝚗 🇦𝚗𝚢𝚊  𓆩×͜×𓆪" // your state name
 global.district = "Dibrugarh" // your district name
 global.timezone = "Asia/Kolkata" // search on google if you don't know the timezone of your country //
 global.instagramId = "3.69_pika" // Your insta ID
@@ -80,7 +80,7 @@ global.Menuimage = fs.readFileSync("./AnyaPikaMedia/theme/Menu/Menuimg.jpg")
 global.thumnnaiIs = fs.readFileSync("./AnyaPikaMedia/theme/Menu/Menuimg2.jpeg")
 
 //--------------- Systemjc Function --------------\\
-global.autoTyping = false 
+global.autoTyping = true 
 global.autoreadpmngc = false 
 global.autoReadGc = false 
 global.autoRecord = false 
